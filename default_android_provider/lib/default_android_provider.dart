53d316b8d2da81b89fa782edc1db8884a4aa416a
ikbalkaya <ceikbal@gmail.com>
2022-01-17 19:27:38 +0000

    Made plugin interface implement the method

--- default_android_provider/lib/default_android_provider.dart
+++ default_android_provider/lib/default_android_provider.dart
@@ -2,20 +2,27 @@ import 'dart:async';
 
 import 'package:default_android_provider/src/timer_push_provider.dart';
 import 'package:flutter/services.dart';
+import 'package:push_provider/ably_compatible_message.dart';
+import 'package:push_provider/push_provider.dart';
 export 'package:default_android_provider/src/timer_push_provider.dart';
 export 'src/timer_push_provider.dart';
 
-class DefaultAndroidProvider {
+class DefaultAndroidProvider extends PushProvider{
   static const MethodChannel _channel =
       MethodChannel('default_android_provider');
   static const EventChannel _pushChannel =
       EventChannel('default_android_provider_event');
 
-  static final TimerPushProvider timerPushProvider = TimerPushProvider(_pushChannel);
+  static final TimerPushProvider _timerPushProvider = TimerPushProvider(_pushChannel);
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  @override
+  Stream<AblyCompatibleMessage> messageStream() {
+    return _timerPushProvider.messageStream();
   }
 
 }
